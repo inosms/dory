4dc415e57a95d6ee9a2dc063e6f85a75b0d3ad08
inosms <kurisumasupurezento@gmail.com>
2015-08-12 16:07:59 +0200

    added min version to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+if sys.version_info[0] < 3:
+    print("python < 3.0 is not supported :(")
+    sys.exit(0)
 
 setup(name='Dory',
-      version='0.0.1',
+      version='0.0.2',
       description='Simple Backup',
       author='inosms',
       url='https://github.com/inosms/dory',
